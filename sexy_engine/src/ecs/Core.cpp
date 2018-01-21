9137130c82343807d429e385b645c29fd3ddee76
Linsey Benoît <benoitlinsey20@gmail.com>
2018-01-21 20:38:00 +0100

    [MERGE]

--- sexy_engine/src/ecs/Core.cpp
+++ sexy_engine/src/ecs/Core.cpp
@@ -49,8 +49,8 @@ unsigned int Sex::Core::getIndexType() const noexcept
 
 void Sex::Core::loadSystemsIn(const std::string &dirName)
 {
-	std::cout << "Opening the dir " <<  (std::string(ROOT_DIR) + "/" + dirName) << std::endl;
-	for (auto &p : std::experimental::filesystem::directory_iterator(std::string(ROOT_DIR) + "/" + dirName)) {
+	std::cout << "Opening the dir " <<  (std::string("ROOT_DIR") + "/" + dirName) << std::endl;
+	for (auto &p : std::experimental::filesystem::directory_iterator(std::string("ROOT_DIR") + "/" + dirName)) {
 		std::cout << "Try to open " << p << " with success" << std::endl;
 		try {
             std::cout << p << std::endl;
